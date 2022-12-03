5f8bf27c85b564982aac4c57dfef304c82112795
allen0524 <william0318563@gmail.com>
2022-12-03 16:21:00 +0800

    Fix:lint-staged file mismatch(re-install all packages)

--- packages/Button/Button.stories.tsx
+++ packages/Button/Button.stories.tsx
@@ -9,4 +9,6 @@ export default {
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+	children: '123',
+};
